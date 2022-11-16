df899cb0243b9e8d59f6aeec25c9aeac42e5189f
mxy <614738304@qq.com>
2022-11-16 18:45:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author mxy
+ * @create 2022-11-16-18:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
